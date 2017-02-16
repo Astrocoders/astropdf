fd016dcb21b176fd9577beec4c2fa8d0074a5330
Gabriel Rubens <grsabreu@gmail.com>
2017-02-16 11:35:34 -0300

    Modify sample to show context example

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import SimplePDF from './PDF'
+import SimplePDFContext from './PDFWithContext'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <SimplePDF ref="pdf"/>
+        <SimplePDFContext ref="pdf"/>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -20,7 +22,7 @@ class App extends Component {
   }
 
   handleClick(){
-    this.refs.pdf.generatePDF()
+    console.log(this.refs.pdf.getJSON())
   }
 }
 
